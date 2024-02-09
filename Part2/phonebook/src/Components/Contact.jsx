d924262a2ab2ca43649d75604f432e1e4c55c850
Conor Kennedy <kennec31@tcd.ie>
2024-02-09 17:32:31 +0000

    Mongo DB

--- Part2/phonebook/src/Components/Contact.jsx
+++
@@ -1,6 +0,0 @@
-const Contact = ({ person, name, number, handleDelete }) => {
-    return (
-    <p>{name} {number} <button onClick={() => handleDelete(person.id)}>delete</button></p>
-    )}
-
-export default Contact
